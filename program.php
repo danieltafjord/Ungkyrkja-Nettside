c54b206d5da97e130c6f991fc5bb4c87cc40e3ac
John Ingve Olsen <johningveolsen@gmail.com>
2015-10-06 11:06:02 +0200

    program: add new navbar

--- program.php
+++ program.php
@@ -1,4 +1,13 @@
 <?php
+	include_once('resources/init.php');
+	include_once('account/login.php');
+
+	if(!empty($_COOKIE['auth-logged'])) {
+		$islogged = false;
+	} else {
+		$islogged = true;
+	}
+
 	# Connect to database
 	$con = mysqli_connect("localhost","ungkyrkja","ungkyrkja","ungkyrkja");
 	$query = mysqli_query($con, "SELECT * FROM uk_program ORDER BY date ASC");
@@ -24,12 +33,85 @@
 <body>
 
 <!-- Header -->
-<div style="width:100%;height:60px;background-color:#fff;">
-	<a href=""><img src="img/uk_logo.png" class="logo-img" /></a>
-	<a href="program.php" style="float:right;margin-right:40px;margin-top:14px;padding:5px;">Program</a>
-	<a href="bilder.php" style="float:right;margin-right:40px;margin-top:14px;padding:5px;">Bilder</a>
-	<a href="" style="float:right;margin-right:40px;margin-top:14px;padding:5px;">Heim</a>
-</div>
+<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <i class="material-icons">more_vert</i><span class="sr-only">Toggle navigation</span>
+	      </button>
+	      <a class="navbar-brand" href="#">Ungkyrkja</a>
+	    </div>
+
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav navbar-right">
+	        <li><a href="index.php">Home</a></li>
+	        <li class="active"><a href="">Program</a></li>
+	        <li><a href="index.php#">Bilder</a></li>
+
+	        <!-- If logged in show username -->
+	        <?php if(!empty($_COOKIE['auth-u'])) : ?>
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php if(!empty($_COOKIE['auth-u'])) {echo $_COOKIE['auth-u'];} ?> <span class="caret"></span></a>
+	          <ul class="dropdown-menu" style="">
+	          <li><a href="account/">Min profil</a></li>
+	            <form style="width:250px;" action="" method="POST">
+				  <button type="submit" name="logout" class="btn btn-primary">Logout</button>
+				</form>
+	          </ul>
+	        </li>
+	        <?php endif; ?>
+
+	        <?php if(empty($_COOKIE['auth-logged'])) :?>
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Login <span class="caret"></span></a>
+	          <ul class="dropdown-menu" style="">
+	            <form style="width:250px;" action="" method="POST">
+				  <div class="form-group">
+				    <label for="exampleInputEmail1">Brukernavn</label>
+				    <input type="text" class="form-control" name="user" placeholder="Brukernavn">
+				  </div>
+				  <div class="form-group">
+				    <label for="exampleInputPassword1">Passord</label>
+				    <input type="password" class="form-control" name="pass" placeholder="Passord">
+				  </div>
+				  <button type="submit" name="login" class="btn btn-primary">Submit</button>
+				</form>
+	          </ul>
+	        </li>
+	    	<?php endif;?>
+	    	<?php if(empty($_COOKIE['auth-logged'])) :?>
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Register <span class="caret"></span></a>
+	          <ul class="dropdown-menu" style="">
+	            <form style="width:250px;" action="" method="POST">
+	            <div class="form-group">
+				    <label for="exampleInputEmail1">Navn</label>
+				    <input type="text" class="form-control" name="name" placeholder="Navn">
+				  </div>
+				  <div class="form-group">
+				    <label for="exampleInputEmail1">Brukernavn</label>
+				    <input type="text" class="form-control" name="user" placeholder="Brukernavn">
+				  </div>
+				  <div class="form-group">
+				    <label for="exampleInputPassword1">Passord</label>
+				    <input type="password" class="form-control" name="pass" placeholder="Passord">
+				  </div>
+				  <div class="form-group">
+				    <label for="exampleInputEmail1">Email</label>
+				    <input type="email" class="form-control" name="email" placeholder="Email">
+				  </div>
+				  <button type="submit" name="register" class="btn btn-primary">Submit</button>
+				</form>
+	          </ul>
+	        </li>
+	    	<?php endif; ?>
+	      </ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+
 
 <!--Main bit-->
 <div class="container-fluid">
