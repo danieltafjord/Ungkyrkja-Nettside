43b37321f14df2af2a36cdc0c1f4d34808976dc2
John Ingve Olsen <johningveolsen@gmail.com>
2015-10-12 22:09:44 +0200

    Edit_Program: Total redesign

--- edit_program.php
+++ edit_program.php
@@ -81,12 +81,84 @@
 <body>
 
 <!-- Header -->
-<div style="width:100%;height:60px;background-color:#fff;">
-	<a href=""><img src="img/uk_logo.png" class="logo-img" /></a>
-	<a href="program.php" style="float:right;margin-right:40px;margin-top:14px;padding:5px;">Program</a>
-	<a href="bilder.php" style="float:right;margin-right:40px;margin-top:14px;padding:5px;">Bilder</a>
-	<a href="" style="float:right;margin-right:40px;margin-top:14px;padding:5px;">Heim</a>
-</div>
+<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <i class="material-icons">more_vert</i><span class="sr-only">Toggle navigation</span>
+	      </button>
+	      <a class="navbar-brand" href="index.php">Ungkyrkja</a>
+	    </div>
+
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav navbar-right">
+	        <li><a href="index.php">Home</a></li>
+	        <li><a href="program.php">Program</a></li>
+	        <li><a href="index.php#">Bilder</a></li>
+
+	        <!-- If logged in show username -->
+	        <?php if(!empty($_COOKIE['auth-u'])) : ?>
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php if(!empty($_COOKIE['auth-u'])) {echo $_COOKIE['auth-u'];} ?> <span class="caret"></span></a>
+	          <ul class="dropdown-menu" style="">
+	          <li><a href="account/">Min profil</a></li>
+	            <form style="width:250px;" action="" method="POST">
+				  <button type="submit" name="logout" class="btn btn-primary">Logout</button>
+				</form>
+	          </ul>
+	        </li>
+	        <?php endif; ?>
+
+	        <?php if(empty($_COOKIE['auth-logged'])) :?>
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Login <span class="caret"></span></a>
+	          <ul class="dropdown-menu" style="">
+	            <form style="width:250px;" action="" method="POST">
+				  <div class="form-group">
+				    <label for="exampleInputEmail1">Brukernavn</label>
+				    <input type="text" class="form-control" name="user" placeholder="Brukernavn">
+				  </div>
+				  <div class="form-group">
+				    <label for="exampleInputPassword1">Passord</label>
+				    <input type="password" class="form-control" name="pass" placeholder="Passord">
+				  </div>
+				  <button type="submit" name="login" class="btn btn-primary">Submit</button>
+				</form>
+	          </ul>
+	        </li>
+	    	<?php endif;?>
+	    	<?php if(empty($_COOKIE['auth-logged'])) :?>
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Register <span class="caret"></span></a>
+	          <ul class="dropdown-menu" style="">
+	            <form style="width:250px;" action="" method="POST">
+	            <div class="form-group">
+				    <label for="exampleInputEmail1">Navn</label>
+				    <input type="text" class="form-control" name="name" placeholder="Navn">
+				  </div>
+				  <div class="form-group">
+				    <label for="exampleInputEmail1">Brukernavn</label>
+				    <input type="text" class="form-control" name="user" placeholder="Brukernavn">
+				  </div>
+				  <div class="form-group">
+				    <label for="exampleInputPassword1">Passord</label>
+				    <input type="password" class="form-control" name="pass" placeholder="Passord">
+				  </div>
+				  <div class="form-group">
+				    <label for="exampleInputEmail1">Email</label>
+				    <input type="email" class="form-control" name="email" placeholder="Email">
+				  </div>
+				  <button type="submit" name="register" class="btn btn-primary">Submit</button>
+				</form>
+	          </ul>
+	        </li>
+	    	<?php endif; ?>
+	      </ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
 
 <!-- Main bit -->
 
@@ -100,30 +172,32 @@
 ?>
 
 <div class="container-fluid">
-	<div class="panel panel-primary">
-		<div class="panel-heading">
-			<form class="title-form form-group">
-				<label for="title">Tittel:</label>
-				<input type="text" class="form-control" id="title" value="<?php echo $title; ?>" />
-			</form>
-		</div>
-		<div class="panel-body">
-			<form class="main-form form-group" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-				<fieldset>
-				<legend><i class="material-icons md-dark">access_time</i> Dato og tid: </legend>
-					<p>Startdato: </p><input type="date" class="form-control" id="date" name="date" value="<?php if($date!=""){echo $date->format("Y-m-d");} ?>" />
-					<p>Starttid: </p><input type="time" class="form-control" id="time" name="time" value="<?php if($date!=""){echo $date->format("H:i");} ?>" />
-					<p>Sluttdato: </p><input type="date" class="form-control" id="enddate" name="enddate" value="<?php if($enddate!=""){echo $enddate->format("Y-m-d");} ?>" />
-					<p>Sluttid: </p><input type="time" class="form-control" id="endtime" name="endtime" value="<?php if($enddate!=""){echo $enddate->format("H:i");}?>" />
-				</fieldset>
-				<legend><i class="material-icons md-dark">label</i> Innhold: </legend>
-				<textarea class="form-control" rows="5" id="content" name="content"><?php echo $content; ?></textarea>
-				<input type="text" class="form-control hidden" id="id" name="id" value="<?php echo $id; ?>" />
-				<input type="text" class="title-input form-control hidden" id="title" name="title" value="" />
-			</form>
+	<div class="row">
+		<div class="col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
+			<div class="well">
+				<form class="main-form form-group" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+					<Legend>Tittel:</legend>
+					<input type="text" class="form-control" id="title" value="<?php echo $title; ?>" name="title"/>
+					<fieldset>
+						<legend>Dato og tid:</legend>
+						<label for="date" class="control-label">Start dato:</label>
+						<input type="date" class="form-control" id="date" name="date" value="<?php if($date!=""){echo $date->format("Y-m-d");} ?>" />
+						<label for="time" class="control-label">Start tid: </label>
+						<input type="time" class="form-control" id="time" name="time" value="<?php if($date!=""){echo $date->format("H:i");} ?>" />
+						<label for="enddate" class="control-label">Slutt dato:</label>
+						<input type="date" class="form-control" id="enddate" name="enddate" value="<?php if($enddate!=""){echo $enddate->format("Y-m-d");} ?>" />
+						<label for="endtime" class="control-label">Slutt tid:</label>
+						<input type="time" class="form-control" id="endtime" name="endtime" value="<?php if($enddate!=""){echo $enddate->format("H:i");}?>" />
+					</fieldset>
+					<legend>Innhold:</legend>
+					<textarea class="form-control" rows="5" id="content" name="content"><?php echo $content; ?></textarea>
+					<input type="text" class="form-control hidden" id="id" name="id" value="<?php echo $id; ?>" />
+					<button type="submit" class="btn btn-primary">Lagre</button>
+					<a href="program.php"><button type="button" class="btn btn-default">Tilbake</button></a>
+				</form>
+			</div>
 		</div>
 	</div>
-	<button type="button" class="button-save btn btn-primary">Lagre</button>
 </div>
 
 <!-- Footer -->
@@ -154,10 +228,5 @@
 		$.material.init();
 	});
 </script>
-<script src="js/edit_program.js"></script>
-
-<?php 
-mysqli_close($con);
-?>
 </body>
 </html>
--- js/edit_program.js
+++
@@ -1,9 +0,0 @@
-var main = function(){
-	$('.button-save').click(function(){
-		var title = $('input[id=title]').first().attr('value');
-		$('.title-input').attr("value", title);
-		$('.main-form').submit();
-	});
-}
-
-$(document).ready(main);
